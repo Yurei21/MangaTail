19fd678a98c0630fc7dc5f84705fe284c0ee263b
Yurei <69757457+Yurei21@users.noreply.github.com>
2024-11-15 00:34:47 +0800

    Main changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,40 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+require('dotenv').config;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const saltRounds = 10;
+
+const db = mysql.createConnection ({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to Mysql');
+});
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) throw err;
+
+        const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+        db.query(query, [username, hash], (err, result) => {
+            if (err) throw err;
+            res.send('User registerd successfully');
+        });
+    });
+});
+
+
